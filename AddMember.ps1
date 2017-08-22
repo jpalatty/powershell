24e650b0fc8fbb95653610b04182ce69d3dfe264
Jason Palatty <jasonp@shubertorg.com>
2017-08-22 13:23:10 -0400

    commiting add member powershell script

---
+++ AddMember.ps1
@@ -0,0 +1,7 @@
+﻿$group = Read-Host "Enter name of group"
+$memberlist = get-content C:\scripts\members.txt
+foreach ($member in $memberlist) {
+    Add-DistributionGroupMember -Identity $group -Member $member
+    }
+
+Get-DistributionGroupMember -Identity $group
